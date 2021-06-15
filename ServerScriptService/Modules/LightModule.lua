9eb9c1f8348fdc15a223a9a19db560d452b57f54
Vivien Frances Pabuna <vfpabuna@gmail.com>
2021-06-15 03:40:26 -0700

    Neon lighting edits

--- ServerScriptService/Modules/LightModule.lua
+++ ServerScriptService/Modules/LightModule.lua
@@ -1,6 +1,7 @@
 local U = require(game:GetService("ServerScriptService"):WaitForChild("Modules"):WaitForChild("UtilityModule"))
+local DimmedNeonColor = Color3.fromRGB(161, 133, 115)
 
-local function GetLightParts(Source)
+local function GetLightParts(Source, Overrides)
 	local Lights = {}
 
 	local function GetAll(Inside)
@@ -9,8 +10,20 @@ local function GetLightParts(Source)
 				GetAll(Child)
 			end
 		elseif Inside:IsA("Model") or Inside.Parent.Name == "Main" or Inside.Name == "MainLight" then
-			for _, Descendant in pairs(Inside:GetDescendants()) do
-				table.insert(Lights, Descendant)
+			if Overrides == nil then
+				if Inside.Parent.Name ~= "Wall" then
+					if Inside.Parent.Name ~= "Cabinet" and Inside.Parent.Name ~= "Stove" then
+						for _, Descendant in pairs(Inside:GetDescendants()) do
+							table.insert(Lights, Descendant)
+						end
+					else
+						if Inside.Name ~= "MainLight" then
+							for _, Descendant in pairs(Inside:GetDescendants()) do
+								table.insert(Lights, Descendant)
+							end
+						end
+					end
+				end
 			end
 			if Inside:FindFirstChild("Neon") == nil then
 				print("\n\n>>>",Inside.Name,"might not be a light!","<<<\n\n")
@@ -26,26 +39,27 @@ end
 local function TurnOn(Target)
 	local LightParts = GetLightParts(Target)
 	for _, Part in pairs(LightParts) do
-		if Part:FindFirstChild("WasANeonPart") then
-			Part.Material = Enum.Material.Neon
+		if Part:FindFirstChild("OriginalColor") then
+			Part.Color = DimmedNeonColor
 		elseif Part:IsA("SpotLight") or Part:IsA("SurfaceLight") or Part:IsA("PointLight") then
 			Part.Brightness = Part:FindFirstChild("OriginalBrightness").Value
 		end
 	end
 end
 
-local function TurnOff(Target)
-	local LightParts = GetLightParts(Target)
+local function TurnOff(Target, Overrides)
+	local LightParts = GetLightParts(Target, Overrides)
 	print("\nLightParts:",#LightParts,"\n")
 	for _, Part in pairs(LightParts) do
 		if Part:IsA("Part") or Part:IsA("MeshPart") then
 			if Part.Material == Enum.Material.Neon then
-				if Part:FindFirstChild("WasANeonPart") == nil then
-					local WasANeonPart = Instance.new("BoolValue")
-					WasANeonPart.Name = "WasANeonPart"
-					WasANeonPart.Parent = Part
+				if Part:FindFirstChild("OriginalColor") == nil then
+					local OriginalColor = Instance.new("Color3Value")
+					OriginalColor.Name = "OriginalColor"
+					OriginalColor.Parent = Part
+					OriginalColor = Part.Color
 				end
-				Part.Material = Enum.Material.Plastic
+				Part.Color = DimmedNeonColor
 			end
 		elseif Part:IsA("SpotLight") or Part:IsA("SurfaceLight") or Part:IsA("PointLight") then
 			if Part:FindFirstChild("OriginalBrightness") == nil then
@@ -55,6 +69,7 @@ local function TurnOff(Target)
 				OriginalBrightness.Value = Part.Brightness
 			end
 			if Part.Parent.Name == "MainLight" then
+				print("MainLight found")
 				Part.Brightness = 0.1
 			else
 				Part.Brightness = 0
@@ -95,11 +110,11 @@ function LightModule:TurnOn(SourceName)
 	Cor()
 end
 
-function LightModule:TurnOff(SourceName)
+function LightModule:TurnOff(SourceName, Overrides)
 	local function Main()
 		local Source = GetSource(SourceName)
 		if Source then
-			TurnOff(Source)
+			TurnOff(Source, Overrides)
 		end
 	end
 	local Cor = coroutine.wrap(Main)
