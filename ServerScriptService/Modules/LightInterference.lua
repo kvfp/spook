ccdcd69fc677409888dca2b3341bd12d1690c916
kvfp <vfpabuna@gmail.com>
2021-07-06 22:30:22 -0700

    Update LightInterference

--- ServerScriptService/Modules/LightInterference.lua
+++ ServerScriptService/Modules/LightInterference.lua
@@ -1,7 +1,7 @@
 local U = require(game:GetService("ServerScriptService"):WaitForChild("Modules"):WaitForChild("UtilityModule"))
 local LightModule = require(U:Import("LightModule"))
 
-local MaxLightDistanceFromPlayer = 20
+local MaxLightDistanceFromPlayer = 100
 
 local LightInterference = {}
 
@@ -17,9 +17,11 @@ function LightInterference:Interfere(Info)
 		local Char = Info.Player.Character
 		if not Char then return end
 		local Root = Char:WaitForChild("HumanoidRootPart")
-		
+
+		print("Final GetLightParts:",#LightParts)
+
 		local Targets = {}
-		
+
 		for _, Part in pairs(LightParts) do
 			if Part:IsA("Part") or Part:IsA("MeshPart") then
 				local DistanceToRoot = (Root.Position-Part.Position).Magnitude
@@ -28,26 +30,34 @@ function LightInterference:Interfere(Info)
 						Part:FindFirstChildOfClass("SurfaceLight") or Part:FindFirstChildOfClass("SpotLight") then
 						table.insert(Targets, Part)
 					end
+				elseif Part.Name == "MainLight" then
+					table.insert(Targets, Part)
 				end
 			end
 		end
 		
+		print("\n\n\n\nTARGETS")
+		for _, Target in pairs(Targets) do
+			print(Target)
+		end
+		print("\n\n\n")
+
 		if Info.Range == "Single" then
-			LightModule:Flicker(Info.Sources[1])
+			LightModule:Flicker({Targets[1]})
 		elseif Info.Range == "All" then
-			LightModule:Flicker(Info.Sources)
+			LightModule:Flicker(Targets)
 		elseif Info.Range == "Partial" then
 			local Indices = {}
 			local PartialList = {}
-			for i = 1, Info.Range do
-				local RandomIndex = math.random(1, #Info.Sources)
+			for i = 1, Info.Count do
+				local RandomIndex = math.random(1, #Targets)
 				if Indices[RandomIndex] ~= nil then
 					repeat
-						RandomIndex = math.random(1, #Info.Sources)
-					until Indices[RandomIndex] == nil
+						RandomIndex = math.random(1, #Targets)
+					until Indices[RandomIndex] == nil or #Targets < Info.Count
 				end
 				Indices[RandomIndex] = 1
-				table.insert(PartialList, LightParts[RandomIndex])
+				table.insert(PartialList, Targets[RandomIndex])
 			end
 			print("PartialList Length:",#PartialList)
 			LightModule:Flicker(PartialList)
